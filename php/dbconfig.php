136e68a6b39a3898d6b9eb324de77c6ff82e8b62
mvanmaele <mvanmaele@talusanalytics.com>
2017-08-04 13:20:00 -0400

    php  tweak

--- php/dbconfig.php
+++ php/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 	// define database connection settings
 	define('MYSQL_HOSTNAME', 'talusdb.cvsrrvlopzxr.us-west-1.rds.amazonaws.com;port=3306');
-	define('MYSQL_DATABASE', 'ghsa_network_analysis');
+	define('MYSQL_DATABASE', 'fluoncall');
 	define('MYSQL_USERNAME', 'jpecht');
 	define('MYSQL_PASSWORD', 'scorecard211');
 ?>
