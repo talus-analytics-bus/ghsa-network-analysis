33c71d3bb8cdc88a1304bea53773b5e8c325075f
mvanmaele <mvanmaele@talusanalytics.com>
2017-09-13 17:53:25 -0400

    Add methods for tagging FA.gov data and processing it into the draft data structure
    *Tagging recipient
    *Tagging function and disease
    *Data validation checks

--- research/data/tools/js/app.js
+++ research/data/tools/js/app.js
@@ -47,11 +47,28 @@ const App = {};
 	};
 
 	App.hashDisease = {
-		"Malaria": "Malaria"
+	  "Health - General": "General",
+	  "Maternal and Child Health": "Maternal and child health",
+	  "Nutrition": "Nutrition",
+	  "Water Supply and Sanitation": "Water supply and sanitation",
+	  "HIV/AIDS": "HIV/AIDS",
+	  "Malaria": "Malaria",
+	  "Other Public Health Threats": "Other",
+	  "Pandemic Influenza and Other Emerging Threats (PIOET)": "Pandemic influenza",
+	  "Tuberculosis": "Tuberculosis"
 	};
 
 	App.hashFunction = {
 		"Project-type interventions": "Project-type interventions"
+	};
+
+	App.hashAidType = {
+	  "Contributions to specific-purpose programmes and funds managed by international organisations (multilateral, INGO)": "Contributions to specific-purpose programmes and funds managed by international organisations (multilateral, INGO)",
+	  "Core contributions to multilateral institutions": "Core contributions to multilateral institutions",
+	  "Donor country personnel": "Donor country personnel",
+	  "Other technical assistance": "Other technical assistance",
+	  "Project-type interventions": "Project-type interventions",
+	  "Sector budget support": "Sector budget support"
 	};
 
 	App.mapDisease = (input) => {
@@ -67,6 +84,36 @@ const App = {};
 		if (outputTmp !== undefined) return outputTmp;
 		else return input; // TODO don't return input here, since it should throw an error
 	};
+
+	App.tagFaRecipient = (record, transaction) => {
+		const specifiesCountry = transaction['Award Transaction - Recipient Country - Name'] !== "";
+		const specifiesRegion = transaction['Award Transaction - Recipient Region - Name'] !== "";
+		if (specifiesCountry) {
+			const recipient_country = transaction['Award Transaction - Recipient Country - Name'];
+			record.recipient_country = recipient_country;
+			record.recipient_sector = "Country";
+			record.recipient_name = recipient_country;
+		} else if (specifiesRegion) {
+			const recipient_region = transaction['Award Transaction - Recipient Region - Name'];
+			if (recipient_region === "Worldwide") {
+				// worldwide case
+				record.recipient_country = 'Multiple countries (unspecified)';
+				record.recipient_sector = "Country";
+				record.recipient_name = 'Multiple countries (unspecified)';
+			} else if (recipient_region === "Developing countries, unspecified") {
+				// multiple countries case without region
+				record.recipient_country = 'Multiple countries (unspecified)';
+				record.recipient_sector = "Country";
+				record.recipient_name = 'Multiple countries (unspecified)';
+			} else {
+				// region specified and really is region
+				record.recipient_country = 'Multiple countries (regional)';
+				record.recipient_sector = "Region";
+				record.recipient_name = recipient_region;
+			}
+		}
+	};
+
 
 	App.mapDataFa = (rawData) => {
 		console.log('record count = ' + rawData.length);
@@ -98,18 +145,20 @@ const App = {};
 				newRecord.donor_sector = "Government";
 				newRecord.donor_name = transaction['Award Accountable Organization - Name'];
 
-				newRecord.recipient_country = recipient_country;
-				newRecord.recipient_sector = "Country";
-				newRecord.recipient_name = recipient_country;
+				// tag recipient country or region
+				App.tagFaRecipient(newRecord, transaction);
 				
-				newRecord.cy_award_start = new Date(transaction['Award Date - Start Date - Date']).getFullYear().toString();
-				newRecord.cy_award_end = new Date(transaction['Award Date - End Date - Date']).getFullYear().toString();
+				const cy_award_start = new Date(transaction['Award Date - Start Date - Date']).getFullYear().toString();
+				const cy_award_end = new Date(transaction['Award Date - Start Date - Date']).getFullYear().toString();
+				newRecord.cy_award_start = (cy_award_start !== "NaN") ? cy_award_start : "";
+				newRecord.cy_award_end = (cy_award_end !== "NaN") ? cy_award_start : "";
 
 				newRecord.source.dataset_name = "ForeignAssistance.gov";
 				newRecord.source.date_added_mmddyyyy = "09122017"; // TODO auto date
 
 				newRecord.project_disease = App.mapDisease(transaction['Award Transaction - Sector']);
-				newRecord.project_function = App.mapFunction(transaction['Award Transaction - Aid Type']);
+				// newRecord.project_function = App.mapFunction(transaction['Award Transaction - Aid Type']);
+				newRecord.project_function = App.hashAidType[transaction['Award Transaction - Aid Type']];
 				newRecord.project_name = transaction['Award Title'];
 				newRecord.project_id = id.toString(); // TODO find better ID to use
 				newRecord.award_id = transaction['Award Identifier']; // TODO find better ID to use
@@ -120,10 +169,13 @@ const App = {};
 
 				this_country_transactions.forEach(curTransaction => {
 					curTransaction.processed = true;
+					let cyForTransaction = new Date(transaction['Award Transaction - Value Date']).getFullYear().toString();
+					if (cyForTransaction === "NaN") cyForTransaction = "";
+					
 					const newTransactionBlob = {
-						type: curTransaction['Award Transaction - Type'].toLowerCase(),
+						type: curTransaction['Award Transaction - Type'].toLowerCase().trim(),
 						amount: parseFloat(curTransaction['Award Transaction - Value']),
-						cy: new Date(transaction['Award Transaction - Value Date']).getFullYear().toString()
+						cy: cyForTransaction
 					};
 					if (newTransactionBlob.type === 'disbursement') {
 						if (total_disbursed_tmp === null) total_disbursed_tmp = newTransactionBlob.amount;
