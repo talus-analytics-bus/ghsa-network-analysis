d7e0f3c6afa957694d165cebdecb817b54a491e0
mvanmaele <mvanmaele@talusanalytics.com>
2017-09-13 11:26:45 -0400

    Bug fix to FA.gov data processing
    Data at the project level weren't being organized correctly

--- research/data/tools/js/app.js
+++ research/data/tools/js/app.js
@@ -2,7 +2,7 @@ const App = {};
 
 (() => {
 	App.test = () => {
-		console.log(faRaw);
+		// console.log(faRaw);
 	};
 
 	/* getData
@@ -15,7 +15,7 @@ const App = {};
 		xhr.setRequestHeader('Content-type', 'application/json');
 		xhr.onload = function(e) {
 			if (this.status == 200) {
-				console.log(JSON.parse(this.response)[0]);
+				// console.log(JSON.parse(this.response)[0]);
 				callback(JSON.parse(this.response)); // do stuff with the returned data
 				// const blob = new Blob([this.response], {type: 'application/json'});
 				// const downloadUrl = URL.createObjectURL(blob);
@@ -71,64 +71,73 @@ const App = {};
 	App.mapDataFa = (rawData) => {
 		console.log('record count = ' + rawData.length);
 
-		// EXAMPLE do AID-680-A-11-00001
-		const proj = rawData.filter(d => d['Award Identifier'] === "AID-680-A-11-00001");
+		// // EXAMPLE do AID-680-A-11-00001
+		// const proj = rawData.filter(d => d['Award Identifier'] === "AID-680-A-11-00001");
 
-		// match award location
-		proj.forEach(transaction => {
-			// get the transactions that match this one's recipient country
-			const recipient_country = transaction['Award Transaction - Recipient Country - Name'];
-			const this_country_transactions = proj.filter(d => d['Award Transaction - Recipient Country - Name'] === recipient_country && d.processed === undefined);
-			
-			if (this_country_transactions.length === 0) return;
-	
+		// get unique project IDs
+		const uniqueProjectIds = _.unique(_.pluck(rawData, 'Award Identifier'));
+		let id = 0;
+		for (let i = 0; i < uniqueProjectIds.length; i++) {
+			const proj = rawData.filter(d => d['Award Identifier'] === uniqueProjectIds[i]);
+			if (proj.length === 0) continue;
+
+			// match award location
 			// write new record			
 			const newRecord = {"project_id":"","project_name":"","project_function":"","project_disease":"","donor_country":"","donor_sector":"","donor_name":"","recipient_country":"","recipient_sector":"","recipient_name":"","cy_award_start":"","cy_award_end":"","total_committed":"","total_disbursed":"","commitments":[],"disbursals":[],"source":{"dataset_name":"","date_added_mmddyyyy":""}};
-			
-			newRecord.donor_country = "United States of America";
-			newRecord.donor_sector = "Government";
-			newRecord.donor_name = transaction['Award Accountable Organization - Name'];
+			id++;
+			proj.forEach(transaction => {
+				// get the transactions that match this one's recipient country
+				const recipient_country = transaction['Award Transaction - Recipient Country - Name'];
+				const this_country_transactions = proj.filter(d => d['Award Transaction - Recipient Country - Name'] === recipient_country && d.processed === undefined);
+				
+				if (this_country_transactions.length === 0) return;
+		
+				
+				newRecord.donor_country = "United States of America";
+				newRecord.donor_sector = "Government";
+				newRecord.donor_name = transaction['Award Accountable Organization - Name'];
 
-			newRecord.recipient_country = recipient_country;
-			newRecord.recipient_sector = "Country";
-			newRecord.recipient_name = recipient_country;
-			
-			newRecord.cy_award_start = new Date(transaction['Award Date - Start Date - Date']).getFullYear().toString();
-			newRecord.cy_award_end = new Date(transaction['Award Date - End Date - Date']).getFullYear().toString();
+				newRecord.recipient_country = recipient_country;
+				newRecord.recipient_sector = "Country";
+				newRecord.recipient_name = recipient_country;
+				
+				newRecord.cy_award_start = new Date(transaction['Award Date - Start Date - Date']).getFullYear().toString();
+				newRecord.cy_award_end = new Date(transaction['Award Date - End Date - Date']).getFullYear().toString();
 
-			newRecord.source.dataset_name = "ForeignAssistance.gov";
-			newRecord.source.date_added_mmddyyyy = "09122017"; // TODO auto date
+				newRecord.source.dataset_name = "ForeignAssistance.gov";
+				newRecord.source.date_added_mmddyyyy = "09122017"; // TODO auto date
 
-			newRecord.project_disease = App.mapDisease(transaction['Award Transaction - Sector']);
-			newRecord.project_function = App.mapFunction(transaction['Award Transaction - Aid Type']);
+				newRecord.project_disease = App.mapDisease(transaction['Award Transaction - Sector']);
+				newRecord.project_function = App.mapFunction(transaction['Award Transaction - Aid Type']);
+				newRecord.project_name = transaction['Award Title'];
+				newRecord.project_id = id.toString(); // TODO find better ID to use
+				newRecord.award_id = transaction['Award Identifier']; // TODO find better ID to use
 
 
-			let total_committed_tmp = null;
-			let total_disbursed_tmp = null;
+				let total_committed_tmp = null;
+				let total_disbursed_tmp = null;
 
-			this_country_transactions.forEach(curTransaction => {
-				curTransaction.processed = true;
-				const newTransactionBlob = {
-					amount: parseFloat(curTransaction['Award Transaction - Value']),
-					cy: new Date(transaction['Award Transaction - Value Date']).getFullYear().toString()
-				};
-				if (curTransaction['Award Transaction - Type'] === 'Disbursement') {
-					newRecord.disbursals.push(newTransactionBlob);
-					if (total_disbursed_tmp === null) total_disbursed_tmp = newTransactionBlob.amount;
-					else total_disbursed_tmp = total_disbursed_tmp + newTransactionBlob.amount;
-				} else if (curTransaction['Award Transaction - Type'] === 'Commitment') {
-					newRecord.commitments.push(newTransactionBlob);
-					if (total_committed_tmp === null) total_committed_tmp = newTransactionBlob.amount;
-					else total_committed_tmp = total_committed_tmp + newTransactionBlob.amount;
-				}
+				this_country_transactions.forEach(curTransaction => {
+					curTransaction.processed = true;
+					const newTransactionBlob = {
+						amount: parseFloat(curTransaction['Award Transaction - Value']),
+						cy: new Date(transaction['Award Transaction - Value Date']).getFullYear().toString()
+					};
+					if (curTransaction['Award Transaction - Type'] === 'Disbursement') {
+						newRecord.disbursals.push(newTransactionBlob);
+						if (total_disbursed_tmp === null) total_disbursed_tmp = newTransactionBlob.amount;
+						else total_disbursed_tmp = total_disbursed_tmp + newTransactionBlob.amount;
+					} else if (curTransaction['Award Transaction - Type'] === 'Commitment') {
+						newRecord.commitments.push(newTransactionBlob);
+						if (total_committed_tmp === null) total_committed_tmp = newTransactionBlob.amount;
+						else total_committed_tmp = total_committed_tmp + newTransactionBlob.amount;
+					}
+				});
+
+				newRecord.total_committed = total_committed_tmp;
+				newRecord.total_disbursed = total_disbursed_tmp;
 			});
-
-			newRecord.total_committed = total_committed_tmp;
-			newRecord.total_disbursed = total_disbursed_tmp;
-
 			console.log(newRecord);
-		});
-
-		console.log(proj);
+		}
 	};
 })();
