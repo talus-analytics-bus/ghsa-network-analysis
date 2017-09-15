d3d1630b8a6c12c2cf706754338739e035a73101
jpecht <me.jefferson@gmail.com>
2017-09-15 11:17:40 -0600

    added pan and zoom to map

--- js/components/map.js
+++ js/components/map.js
@@ -8,18 +8,37 @@ const Map = {};
 	 */
 	Map.createWorldMap = (selector, callback) => {
 		// prepare map
-		const mapWidth = 1200;
-		const mapHeight = 600;
+		const width = 1200;
+		const height = 600;
+		const scale = 170;
 
+		// define projection and path
 		const projection = d3.geoMercator()
-			.scale(170)
-			.translate([mapWidth / 2, mapHeight / 2])
+			.translate([width / 2, height / 2])
+			.scale(scale)
 			.precision(.1);
 		const path = d3.geoPath().projection(projection);
+
+		// define zoom
+		const zoom = d3.zoom()
+			.scaleExtent([1, 8])
+			.on('zoom', zoomed);
+
+		// set map width and height
 		const svg = d3.selectAll(selector).append('svg')
-			.attr('width', mapWidth)
-			.attr('height', mapHeight)
+			.attr('width', width)
+			.attr('height', height)
 			.append('g');
+		const g = svg.append('g');
+
+		// add overlay
+		svg.append('rect')
+			.attr('class', 'overlay')
+			.attr('width', width)
+			.attr('height', height);
+
+		// attach zoom
+		svg.call(zoom);
 
 		// add world data
 		d3.json('data/world-50m.json', (error, world) => {
@@ -27,18 +46,31 @@ const Map = {};
 
 			const countries = topojson.feature(world, world.objects.countries).features;
 
-			svg.selectAll('.country')
+			g.selectAll('.country')
 				.data(countries)
 				.enter().append('path')
 					.attr('class', 'country')
 					.attr('d', path);
-			svg.append('path')
+			g.append('path')
 				.datum(topojson.mesh(world, world.objects.countries, (a, b) => a !== b))
 				.attr('class', 'boundary')
 				.attr('d', path);
 
 			if (callback) callback();
 		});
+
+		let currScale = 1;
+		function zoomed() {
+			const transform = d3.event.transform;
+			if (transform.k === currScale) {
+				// pan event; don't use transition
+				g.attr('transform', transform);
+			} else {
+				// zoom event; use transition
+				g.transition().attr('transform', transform);
+				currScale = transform.k;
+			}
+		}
 
 		return { svg, path, projection };
 	};
