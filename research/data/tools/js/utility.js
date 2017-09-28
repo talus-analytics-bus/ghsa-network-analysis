14e347491a63ade688c98f70104bd93303861bd7
mvanmaele <mvanmaele@talusanalytics.com>
2017-09-28 10:55:31 -0400

    rmv functions unused

--- research/data/tools/js/utility.js
+++ research/data/tools/js/utility.js
@@ -1,52 +1,17 @@
+// utility.js
+// Mike Van Maele, Talus Analytics LLC
+// 28 Sep 2017
+//
+// Contains utility functions for Canyonlands data processing.
+// *Hash tables for function/disease tags and IATI publishers, codes, and similar
+// *Conversion and calculation functions that are frequently reused
+
 const Util = {};
 
 Util.calculateAge = (birthday) => { // birthday is a date
     var ageDifMs = Date.now() - birthday.getTime();
     var ageDate = new Date(ageDifMs); // miliseconds from epoch
     return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
-
-Util.JSON2CSV = (objArray) => {
-    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-
-    var str = '';
-    var line = '';
-
-    if (true) {
-        var head = array[0];
-        if ($("#quote").is(':checked')) {
-            for (var index in array[0]) {
-                var value = index + "";
-                line += '"' + value.replace(/"/g, '""') + '",';
-            }
-        } else {
-            for (var index in array[0]) {
-                line += index + ',';
-            }
-        }
-
-        line = line.slice(0, -1);
-        str += line + '\r\n';
-    }
-
-    for (var i = 0; i < array.length; i++) {
-        var line = '';
-
-        if (true) {
-            for (var index in array[i]) {
-                var value = array[i][index] + "";
-                line += '"' + value.replace(/"/g, '""') + '",';
-            }
-        } else {
-            for (var index in array[i]) {
-                line += array[i][index] + ',';
-            }
-        }
-
-        line = line.slice(0, -1);
-        str += line + '\r\n';
-    }
-    return str;
 }
 
 // Save output for the console as JSON
