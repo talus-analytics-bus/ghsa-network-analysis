4bf418c672e6030aeaa4dc76af62c2da0feb84de
mvanmaele <mvanmaele@talusanalytics.com>
2017-10-13 14:45:14 -0400

    new data

--- research/data/tools/data/data prep.js
+++ research/data/tools/data/data prep.js
@@ -1,6 +1,7 @@
 output = [];
 allData.forEach(project => {
 	var obj = {};
+	obj.unique_id = project.project_id
 	obj.project_title = project.project_name;
 	obj.core_capacities = project.core_capacities.join('; ');
 	obj.desc_trns = project.desc_trns;
@@ -9,7 +10,9 @@ allData.forEach(project => {
 	obj.iati_id = project.source.id;
 	
 	// get sector codes
-	
+	var activities_for_proj = iatiActivities.filter(d => d.aid === project.source.id);
+	var curSectorCodes = _.unique(_.pluck(activities_for_proj, 'sector_code'));
+	obj.sector_codes = curSectorCodes.join('; ');
 	output.push(obj);
 });
 copy(output)
