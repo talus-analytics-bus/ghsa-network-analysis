bb95e1cc452cf52f7c3708a8ba9ff79d0d75a9bb
jpecht <me.jefferson@gmail.com>
2017-10-30 08:44:50 -0600

    cleaned up upload code; added tagging

--- js/pages/submit.js
+++ js/pages/submit.js
@@ -22,7 +22,7 @@
 			const lastName = $('.last-name-input').val();
 			const org = $('.org-input').val();
 			const email = $('.email-input').val();
-			/*if (!firstName) {
+			if (!firstName) {
 				noty({ text: 'Please enter a first name before proceeding.' });
 				return;
 			}
@@ -37,14 +37,18 @@
 			if (!email) {
 				noty({ text: 'Please enter an email address before proceeding.' });
 				return;
-			}*/
+			}
 
 			const input = d3.select('.input-report-upload').node();
 			if ('files' in input) {
-				if (!input.files.length) return;  // check that file was uploaded
-				const file = input.files[0];
+				// check that file was uploaded
+				if (!input.files.length) {
+					noty({ text: '<b>Please select a file before proceeding!</b>' });
+					return;
+				}
 
 				// check that file is in xls or xlsx format
+				const file = input.files[0];
 				const fileNameArr = file.name.split('.');
 				const fileType = fileNameArr[fileNameArr.length - 1];
 				if (fileType !== 'xlsx' && fileType !== 'xls') {
@@ -63,38 +67,37 @@
 
 	// parses project report and saves data
 	function submitProjectReport(file) {
-		const xhr = new XMLHttpRequest();
-		xhr.open('GET', `/upload-s3?file-name=${file.name}&file-type=${file.type}`);
-		xhr.onreadystatechange = () => {
-			if (xhr.readyState === 4) {
-				if (xhr.status === 200) {
-					const response = JSON.parse(xhr.responseText);
-					uploadFile(file, response.signedRequest, response.url);
-				} else {
-					noty({ type: 'error', text: 'Could not get signed URL.' });
-				}
-			}
-		};
-		xhr.send();
-	}
+		NProgress.start();
 
-	function uploadFile(file, signedRequest, url) {
-		console.log(signedRequest);
-		const xhr = new XMLHttpRequest();
-		xhr.open('PUT', signedRequest);
-		xhr.onreadystatechange = () => {
-			console.log(xhr);
-			if (xhr.readyState === 4) {
-				if (xhr.status === 200) {
-					console.log(xhr);
-					// TODO
-					noty({ type: 'success', text: 'Report has been submitted!' });
-				} else {
-					console.log(xhr.response);
-					noty({ text: 'There was an issue submitting your report. Please notify the administrators of the tool.' });
-				}
-			}
-		};
-		xhr.send(file);
+		// create form
+		const formData = new FormData();
+		formData.append('firstname', $('.first-name-input').val());
+		formData.append('lastname', $('.last-name-input').val());
+		formData.append('org', $('.org-input').val());
+		formData.append('email', $('.email-input').val());
+		formData.append('upload', file, file.name);
+
+		// send HTTP request
+		$.ajax({
+			url: '/upload-s3',
+			method: 'POST',
+			data: formData,
+			processData: false,
+			contentType: false,
+		})
+			.done(() => {
+				// clear out input
+				$('.input-report-upload').val('');
+				$('.file-name-text').text('No file selected');
+
+				// show success notification
+				noty({ type: 'success', text: '<b>Success!</b><br>Report has been submitted!' });
+				NProgress.done();
+			})
+			.fail((xhr, status) => {
+				console.log(status);
+				noty({ text: '<b>Error!</b><br>There was an issue submitting your report. Please notify the administrators of the tool.' });
+				NProgress.done();
+			});
 	}
 })();
--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var path = require('path');
+var fs = require('fs');
+
 var aws = require('aws-sdk');
-var S3_BUCKET = process.env.S3_BUCKET;
+aws.config.loadFromPath('./config/config.json');
+var s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+var formidable = require('formidable');
 
 // if no hash, send to index
 app.get('/', function(req, res) {
@@ -21,6 +26,7 @@ app.get(/^(.+)$/, function(req, res) {
 		s3.getSignedUrl('putObject', {
 			Bucket: S3_BUCKET,
 			Key: fileName,
+			Body: '',
 			Expires: 60,
 			ContentType: fileType,
 			ACL: 'public-read',
@@ -40,7 +46,44 @@ app.get(/^(.+)$/, function(req, res) {
 	} else {
 		res.sendFile(path.join(__dirname, '/', req.params[0]));
 	}
-});	
+});
+
+app.post('/upload-s3', function(req, res) {
+	var form = new formidable.IncomingForm();
+
+	form.on('error', function(err) {
+		console.log('Error processing file...', err);
+	});
+
+	// parse form and upload to S3 bucket
+	form.parse(req, function(err, fields, files) {
+		// construct tags
+		var tags = 'firstname=' + fields.firstname;
+		tags += '&lastname=' + fields.lastname;
+		tags += '&organization=' + fields.org;
+		tags += '&email=' + fields.email;
+
+		var file = files.upload;
+		console.log(file);
+		var uploadParams = {
+			Bucket: 'ghs-tracking-dashboard',
+			Tagging: tags,
+			Key: file.name,
+			Body: '',
+		};
+		var fileStream = fs.createReadStream(file.path);
+		fileStream.on('error', (err) => {
+			console.log('File Error', err);
+		});
+		uploadParams.Body = fileStream;
+
+		s3.upload(uploadParams, (err, data) => {
+			if (err) console.log('Error uploading to S3', err);
+			if (data) console.log('Success uploading to S3', data.Location);
+		});
+		res.status(200).json([]);
+	});
+});
 
 // Start the HTTP Server
 server.listen(process.env.PORT || 8888, function() {
