aa74f9d0bd56daf628df58dbf68cfd46ff41fc5e
mevanmaele <mvanmaele@talusanalytics.com>
2018-01-04 16:52:50 -0500

    Add option to disable S3 so Mike's Mac doesn't crash

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ var server = require('http').Server(app);
 var path = require('path');
 var fs = require('fs');
 
-var aws = require('aws-sdk');
-aws.config.loadFromPath('./config/config.json');
-var s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 var formidable = require('formidable');
 
@@ -20,65 +17,76 @@ app.get(/^(.+)$/, function(req, res) {
 	res.sendFile(path.join(__dirname, '/', req.params[0]));
 });
 
-// uploading report to s3 bucket
-app.post('/upload-s3', function(req, res) {
-	var form = new formidable.IncomingForm();
+// If enableS3 is true, then the aws-sdk package will be loaded (can cause some
+// Mac machines to crash). This package is required for the "Submit Data" page
+// and functions on that page will fail unless it is true.
+var enableS3 = false;
+if (enableS3) {
+	var aws = require('aws-sdk');
+	aws.config.loadFromPath('./config/config.json');
+	var s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
-	form.on('error', function(err) {
-		console.log('Error processing file...', err);
-	});
+	// uploading report to s3 bucket
+	app.post('/upload-s3', function(req, res) {
+		var form = new formidable.IncomingForm();
 
-	// parse form and upload to S3 bucket
-	form.parse(req, function(err, fields, files) {
-		var file = files.upload;
-
-		// construct tags
-		var tags = 'firstname=' + fields.firstname;
-		tags += '&lastname=' + fields.lastname;
-		tags += '&organization=' + fields.org;
-		tags += '&email=' + fields.email;
-
-		// validate file type and size
-		var fileNameArr = file.name.split('.');
-		var fileType = fileNameArr[fileNameArr.length - 1];
-		if (fileType !== 'xls' && fileType !== 'xlsx') {
-			res.status(500).json({ error: 'File type is not valid: ' + fileType });
-			return;
-		}
-		if (file.size > 100e6) {
-			res.status(500).json({ error: 'File size is over 100 MB: ' + file.size });
-			return;
-		}
-
-		// set up parameters object
-		console.log(file);
-		var uploadParams = {
-			Bucket: 'ghs-tracking-dashboard',
-			Tagging: tags,
-			Key: path.basename(file.path) + '.' + fileType,
-			Body: '',
-		};
-
-		// read file
-		var fileStream = fs.createReadStream(file.path);
-		fileStream.on('error', (err) => {
-			console.log('File Error', err);
+		form.on('error', function(err) {
+			console.log('Error processing file...', err);
 		});
-		uploadParams.Body = fileStream;
 
-		// start upload to s3
-		s3.upload(uploadParams, (err, data) => {
-			if (err) {
-				console.log('Error uploading to S3: ', err);
-				res.status(500).json({ error: err });
+		// parse form and upload to S3 bucket
+		form.parse(req, function(err, fields, files) {
+			var file = files.upload;
+
+			// construct tags
+			var tags = 'firstname=' + fields.firstname;
+			tags += '&lastname=' + fields.lastname;
+			tags += '&organization=' + fields.org;
+			tags += '&email=' + fields.email;
+
+			// validate file type and size
+			var fileNameArr = file.name.split('.');
+			var fileType = fileNameArr[fileNameArr.length - 1];
+			if (fileType !== 'xls' && fileType !== 'xlsx') {
+				res.status(500).json({ error: 'File type is not valid: ' + fileType });
+				return;
 			}
-			if (data) {
-				console.log('Success uploading to S3: ', data.Location);
-				res.status(200).json({ location: data.Location });
+			if (file.size > 100e6) {
+				res.status(500).json({ error: 'File size is over 100 MB: ' + file.size });
+				return;
 			}
+
+			// set up parameters object
+			console.log(file);
+			var uploadParams = {
+				Bucket: 'ghs-tracking-dashboard',
+				Tagging: tags,
+				Key: path.basename(file.path) + '.' + fileType,
+				Body: '',
+			};
+
+			// read file
+			var fileStream = fs.createReadStream(file.path);
+			fileStream.on('error', (err) => {
+				console.log('File Error', err);
+			});
+			uploadParams.Body = fileStream;
+
+			// start upload to s3
+			s3.upload(uploadParams, (err, data) => {
+				if (err) {
+					console.log('Error uploading to S3: ', err);
+					res.status(500).json({ error: err });
+				}
+				if (data) {
+					console.log('Success uploading to S3: ', data.Location);
+					res.status(200).json({ location: data.Location });
+				}
+			});
 		});
 	});
-});
+} 
+
 
 // Start the HTTP Server
 server.listen(process.env.PORT || 8888, function() {
