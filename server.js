e9405f466884595e7accbef7a508438550492265
Rob Fink <rfink@talusanalytics.com>
2018-03-30 09:42:44 -0700

    Fixed a styling problem on the source-text within the show all funds page.

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ if (enableS3 === 'true') {
 
 
 // Start the HTTP Server
-server.listen(process.env.PORT || 8800, function() {
+server.listen(process.env.PORT || 8805, function() {
 	console.log('Server set up!');
 	console.log(server.address());
 });
