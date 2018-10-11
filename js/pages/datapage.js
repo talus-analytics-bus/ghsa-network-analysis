de41b5980de972bd1d82a1267521fc47e50cd529
Zoe Farmer <zfarmer@talusanalytics.com>
2018-10-11 16:29:06 -0600

    Crude download in place

--- js/pages/datapage.js
+++ js/pages/datapage.js
@@ -1,12 +1,48 @@
 (() => {
-	App.initData = () => {
-		populateTable();
+	let data;
 
+	App.initData = () => {
+		data = App.fundingData
+			.map(d => [
+				(d.project_name || 'Not Reported').toString(),
+				(d.donor_name || 'Not Reported').toString(),
+				(d.recipient_name || 'Not Reported').toString(),
+			]);
+
+		$('.download-container').text(`Download all available data (${Util.comma(data.length)} records)`)
+			.on('click', () => {
+				download("fundingData.json", JSON.stringify(App.fundingData));
+			});
+
+		populateTable();
+	};
+
+	const download = (filename, text) => {
+		// https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
+		var element = document.createElement('a');
+		element.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(text));
+		element.setAttribute('download', filename);
+
+		element.style.display = 'none';
+		document.body.appendChild(element);
+
+		element.click();
+
+		document.body.removeChild(element);
+	};
+
+	const populateTable = () => {
 		$('table.download-data-table').DataTable({
+			data,
+			columns: [
+				{ title: 'Project' },
+				{ title: 'Funder' },
+				{ title: 'Recipient' },
+			],
 			pageLength: 50,
 			scrollCollapse: false,
 			autoWidth: true,
-			ordering: false,
+			ordering: true,
 			searching: true,
 			// pagingType: 'simple',
 			// order: [[1, 'asc']],
@@ -15,23 +51,5 @@
 			// ],
 			bLengthChange: false,
 		});
-	};
-
-	const populateTable = () => {
-		const table = d3.selectAll('table.download-data-table tbody');
-
-		const rows = table.selectAll('tr')
-			.data(App.fundingData)
-			.enter()
-			.append('tr');
-
-		rows.append('td')
-			.text(d => d.project_name);
-
-		rows.append('td')
-			.text(d => d.donor_name);
-
-		rows.append('td')
-			.text(d => d.recipient_name);
 	}
 })();
