9fc851e85238e18a3b51a0ed8b9defe7bcad7852
Zoe Farmer <zfarmer@talusanalytics.com>
2018-10-11 15:45:58 -0600

    well it works... soo poorly

--- js/pages/datapage.js
+++ js/pages/datapage.js
@@ -1,5 +1,37 @@
 (() => {
 	App.initData = () => {
+		populateTable();
 
+		$('table.download-data-table').DataTable({
+			pageLength: 50,
+			scrollCollapse: false,
+			autoWidth: true,
+			ordering: false,
+			searching: true,
+			// pagingType: 'simple',
+			// order: [[1, 'asc']],
+			// columnDefs: [
+			// 	{ targets: [1,2,3], orderable: false},
+			// ],
+			bLengthChange: false,
+		});
 	};
+
+	const populateTable = () => {
+		const table = d3.selectAll('table.download-data-table tbody');
+
+		const rows = table.selectAll('tr')
+			.data(App.fundingData)
+			.enter()
+			.append('tr');
+
+		rows.append('td')
+			.text(d => d.project_name);
+
+		rows.append('td')
+			.text(d => d.donor_name);
+
+		rows.append('td')
+			.text(d => d.recipient_name);
+	}
 })();
